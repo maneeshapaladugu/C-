b01479e08161bbd69745d412e8e3b7140ffb5c83
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2021-05-23 17:58:13 +0530

    Add files via upload

---
+++ Design Patterns/Singleton Design Pattern/Singleton.cpp
@@ -0,0 +1,35 @@
+/*
+  Program to implement Singleton design pattern
+
+  Singleton is a creational design pattern that lets you ensure that a class has only one instance,
+  while providing a global access point to this instance.
+
+  The Singleton class defines the 'GetInstance' method that serves as an alternative
+  to constructor and lets clients access the same instance of this class over and over.
+*/
+
+#include <iostream>
+#include "Singleton.h"
+
+/*
+Static members must be declared inside the class and defined outside the class
+since static members are not part of object.
+*/
+Singleton* Singleton::m_instance(nullptr);
+
+Singleton::Singleton(const std::string valueArg) : m_value(valueArg) {
+}
+
+Singleton::~Singleton() {
+}
+
+std::string Singleton::getValue() const {
+  return m_value;
+}
+
+Singleton* Singleton::getInstance(const std::string& value) {
+  if (m_instance == nullptr) {
+    m_instance = new Singleton(value);
+  }
+  return m_instance;
+}
---
+++ Design Patterns/Singleton Design Pattern/Singleton.h
@@ -0,0 +1,31 @@
+#ifndef Singleton_H
+#define Singleton_H
+
+class Singleton {
+  /*
+    The Singleton's constructor should always be private to prevent direct
+    construction calls with the 'new' operator.
+  */
+private:
+
+  static Singleton* m_instance;
+
+  std::string m_value;
+
+  Singleton(const std::string valueArg);//Constructor
+
+public:
+
+  ~Singleton();//Destructor
+
+  /*
+    This is the static method that controls the access to singleton instance. On the first run,
+    it creates a singleton object and places it into the static field. On subsequent runs,
+    it returns the client existing object stored in the static field.
+  */
+
+  static Singleton* getInstance(const std::string& value);
+
+  std::string getValue() const;
+};
+#endif
